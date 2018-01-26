f8df053b58c566a1f1b7004333fbbf926f23c919
zachalexander <zacharya@cep.org>
2018-01-26 17:19:36 -0500

    cleaning route file

--- routes/targets.js
+++ routes/targets.js
@@ -4,6 +4,8 @@ const Targets = require('../models/targets');
 const config = require('../config/database');
 
 module.exports = (router) => {
+
+  // Add new company information to database
   router.post('/newTarget', (req, res) => {
     if(!req.body.companyName){
       res.json({success: false, message: 'Company name is required.'});
@@ -11,19 +13,19 @@ module.exports = (router) => {
         if(!req.body.location){
         res.json({success: false, message: 'Company location is required.'});
         } else {
-          const newTarget = new Targets ({
-            companyName: req.body.companyName,
-            location: req.body.location
-        });
-        newTarget.save((err) => {
-          if(err) {
-            res.json({success: false, message: err})
-        } else {
-          res.json({success: true, message: 'Company Name saved!'});
+            const newTarget = new Targets ({
+              companyName: req.body.companyName,
+              location: req.body.location
+            });
+            newTarget.save((err) => {
+              if(err) {
+                res.json({success: false, message: err})
+            } else {
+              res.json({success: true, message: 'Company Name saved!'});
+            }
+          });
         }
-      });
-    }
-  }
-});
+      }
+  });
   return router;
 }
