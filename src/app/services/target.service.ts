7720fac41ad2dcca8c6ffd93eeaf464a67a2f708
zachalexander <zacharya@cep.org>
2018-01-31 21:31:03 -0500

    adds responsive design to form

--- src/app/services/target.service.ts
+++ src/app/services/target.service.ts
@@ -13,35 +13,35 @@ export class TargetService {
   submitTarget(target){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('mongodb://zdalexander:Biology512@ds221148.mlab.com:21148/targets/newTarget', target, {headers: headers})
+    return this.http.post('http://localhost:8080/targets/newTarget', target, {headers: headers})
       .map(res => res.json());
   }
 
   getAllTargets(){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('mongodb://zdalexander:Biology512@ds221148.mlab.com:21148/targets/allTargets', {headers: headers})
+    return this.http.get('http://localhost:8080/targets/allTargets', {headers: headers})
       .map(res => res.json());
   }
 
   getSingleTarget(id) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.get('mongodb://zdalexander:Biology512@ds221148.mlab.com:21148/targets/singleTarget/' + id, {headers: headers})
+    return this.http.get('http://localhost:8080/targets/singleTarget/' + id, {headers: headers})
       .map(res => res.json());
   }
 
   editTarget(target){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.put('mongodb://zdalexander:Biology512@ds221148.mlab.com:21148/targets/updateTarget/', target, {headers: headers})
+    return this.http.put('http://localhost:8080/targets/updateTarget/', target, {headers: headers})
       .map(res => res.json());
   }
 
   deleteTarget(id){
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.delete('mongodb://zdalexander:Biology512@ds221148.mlab.com:21148/targets/deleteTarget/'+ id, {headers: headers})
+    return this.http.delete('http://localhost:8080/targets/deleteTarget/'+ id, {headers: headers})
       .map(res => res.json());
   }
 
