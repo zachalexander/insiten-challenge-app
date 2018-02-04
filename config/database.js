b78c867922cbbd34c02eca61a7c646536d6b50b1
zachalexander <zacharya@cep.org>
2018-02-04 17:25:05 -0500

    bootstrap fixes 2

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-  uri: 'mongodb://localhost:27017/insiten-targets',
-  // uri: 'mongodb://zdalexander:Biology512@ds221148.mlab.com:21148/insiten-challenge-app', //production database
+  // uri: 'mongodb://localhost:27017/insiten-targets',
+  uri: 'mongodb://zdalexander:Biology512@ds221148.mlab.com:21148/insiten-challenge-app', //production database
   secret: crypto,
   db: 'insiten-targets'
 }
